7545f4f71051dfc4ed28bd492f1478555c0040c6
Lukáš Stuchlík <stuchlik1@seznam.cz>
2018-12-09 01:31:57 +0100

    Progress on HOG

--- src/playground/hog.py
+++ src/playground/hog.py
@@ -2,133 +2,120 @@ import cv2 as cv
 import numpy as np
 import os
 
-height = 160
-width = 60
+DEBUG = False
+
+def getCroppedSize(cellSize, realSize):
+    """
+    Crops the real image size to be in multiples of cell size
+
+    :param cellSize: HOG cell size (width, height)
+    :type cellSize: (int, int)
+    :param realSize: Real image size (width, height)
+    :type realSize: (int, int)
+    :return: Image size cropped to be multiple of cell size (width, height)
+    :rtype: (int, int)
+    """
+    cellWidth, cellHeight = cellSize
+    width, height = realSize
+    return width // cellWidth * cellWidth, height // cellHeight * cellHeight
+
+def getSubsetsFromImage(partSize, imageSize):
+    """
+    Returns an iterator generating all possible points where a part can be within an image
+    :param partSize: Size of a searched part (width, height)
+    :type partSize: (int, int)
+    :param imageSize: Size of the image (width, height)
+    :type imageSize: (int, int)
+    :return: Tuple (startX, startY, endX, endY) for each possible subset
+    :rtype: (int, int, int, int)
+    """
+    pW, pH = partSize
+    iW, iH = imageSize
+    y = 0
+    while y + pH < iH:
+        x = 0
+        while x + pW < iW:
+            yield x, y, x + pW, y + pH
+            x = x + 1
+        y = y + 1
+
+def getSizeFromShape(shape):
+    """Returns tuple (width, height) from shape (which is usually height, width)"""
+    return shape[1], shape[0]
 
 # parametry pro HOGDescriptor
-cellSide = 12
-cellSize = (cellSide, cellSide)     # h x w
-blockSize = (cellSide * 2, cellSide * 2)    # h x w
+cellSide = 4
+cellSize = (cellSide, cellSide)  # w x h
+blockSize = (cellSide * 2, cellSide * 2)  # w x h
 blockStride = cellSize
-winSize = (width // cellSize[1] * cellSize[1], height // cellSize[0] * cellSize[0])
-nbins = 9
+nBins = 9
 # defaultne nastavene parametry
 derivAperture = 1
 winSigma = -1.
 histogramNormType = 0
 L2HysThreshold = 0.2
 gammaCorrection = 1
-nlevels = 64
+nLevels = 64
 signedGradients = True
 
-positiveTrainDir = "trainBig/positive"
-negativeTrainDir = "trainBig/negative"
-positiveTestDir = "testBig/positive"
-negativeTestDir = "testBig/negative"
+imagesDir = "../../data/images"
+partsDir = imagesDir + "/testing/parts"
+originalDir = imagesDir + "/original/300x300"
 
-positiveTrainCount = len(os.listdir(positiveTrainDir))
-negativeTrainCount = len(os.listdir(negativeTrainDir))
-positiveTestCount = len(os.listdir(positiveTestDir))
-negativeTestCount = len(os.listdir(negativeTestDir))
+partsCount = len(os.listdir(partsDir))
+originalCount = len(os.listdir(originalDir))
 
-maxSVMiterations = 1000
-epsilon = 1e-6
+for i, filename in enumerate(os.listdir(partsDir)):
+    if DEBUG:
+        print(f"Current file: {partsDir}/{filename}")
+    # load a part
+    img = cv.imread(f"{partsDir}/{filename}", 0)
+    partSize = getSizeFromShape(img.shape)
+    if DEBUG:
+        print(f"Part width: {partSize[0]}, height: {partSize[1]}")
+    # resize it to multiple of cellSize
+    croppedSize = getCroppedSize(cellSize, partSize)
+    if DEBUG:
+        print(f"Cropped width: {croppedSize[0]}, height: {croppedSize[1]}")
+    img = cv.resize(img, croppedSize)
+    # construct a HOG descriptor for given size
+    hog = cv.HOGDescriptor(
+        croppedSize,
+        blockSize,
+        blockStride,
+        cellSize,
+        nBins,
+        derivAperture,
+        winSigma,
+        histogramNormType,
+        L2HysThreshold,
+        gammaCorrection,
+        nLevels,
+        signedGradients
+    )
+    partDescriptor = hog.compute(img)
+    print(f"Part descriptor shape: {partDescriptor.shape}")
+    bestResult = {
+        "distance": float("inf"),    # default to +infinity
+        "file": "",
+        "x": -1,
+        "y": -1,
+        "dX": croppedSize[0],
+        "dY": croppedSize[1]
+    }
 
-realWidth, realHeight = winSize
-positives = np.empty([positiveTrainCount, realHeight, realWidth])
-label_positive = np.ones([positiveTrainCount])
-negatives = np.empty([negativeTrainCount, realHeight, realWidth])
-label_negative = np.zeros([negativeTrainCount])
-label_negative[:] = -1
-
-for i, filename in enumerate(os.listdir(positiveTrainDir)):
-    img = cv.imread(f"{positiveTrainDir}/{filename}", 0)
-    # resize na stejnou velikost jako je winSize pro HOG
-    img = cv.resize(img, winSize)
-    positives[i] = img
-
-for i, filename in enumerate(os.listdir(negativeTrainDir)):
-    img = cv.imread(f"{negativeTrainDir}/{filename}", 0)
-    img = cv.resize(img, winSize)
-    negatives[i] = img
-
-trainDataImg = np.concatenate((positives, negatives))
-labels = np.concatenate((label_positive, label_negative))
-
-hog = cv.HOGDescriptor(winSize,
-                        blockSize,
-                        blockStride,
-                        cellSize,
-                        nbins,
-                        derivAperture,
-                        winSigma,
-                        histogramNormType,
-                        L2HysThreshold,
-                        gammaCorrection,
-                        nlevels,
-                        signedGradients)
-trainData = np.empty([positiveTrainCount + negativeTrainCount, hog.getDescriptorSize(), 1])
-trainDataImg = np.uint8(trainDataImg)
-for i, img in enumerate(trainDataImg):
-    trainData[i] = hog.compute(img)
-
-trainData = np.float32(trainData.reshape(positiveTrainCount + negativeTrainCount, hog.getDescriptorSize()))
-labels = np.int32(labels)
-
-svm = cv.ml.SVM_create()
-svm.setType(cv.ml.SVM_C_SVC)
-# default kernel je RBF
-svm.setTermCriteria((cv.TERM_CRITERIA_COUNT, maxSVMiterations, epsilon))
-svm.trainAuto(trainData, cv.ml.ROW_SAMPLE, labels)
-
-test_positive = np.empty([positiveTestCount, 1, hog.getDescriptorSize()])
-for i, filename in enumerate(os.listdir(positiveTestDir)):
-    img = cv.imread(f"{positiveTestDir}/{filename}", 0)
-    img = cv.resize(img, winSize)
-    components = hog.compute(img)
-    components = components.reshape(1, hog.getDescriptorSize())
-    test_positive[i] = components
-
-test_negative = np.empty([negativeTestCount, 1, hog.getDescriptorSize()])
-for i, filename in enumerate(os.listdir(negativeTestDir)):
-    img = cv.imread(f"{negativeTestDir}/{filename}", 0)
-    img = cv.resize(img, winSize)
-    components = hog.compute(img)
-    components = components.reshape(1, hog.getDescriptorSize())
-    test_negative[i] = components
-
-# print("Positive classification times", positiveClassificationTimes, "Negative classification times", negativeClassificationTimes)
-test_positive = np.float32(test_positive)
-test_negative = np.float32(test_negative)
-
-correctPositive = 0
-for i, sample in enumerate(test_positive):
-    result = svm.predict(sample)[1]
-    # index v test_positive by mel presne souhlasit s indexem souboru v tom adresari
-    img = cv.imread(f"{positiveTestDir}/{os.listdir(positiveTestDir)[i]}")
-    if result == 1:
-        correctPositive += 1
-        cv.imwrite(f"resultsHOG/correct/positive_{i}.jpg", img)
-    else:
-        cv.imwrite(f"resultsHOG/incorrect/should_be_positive_{i}.jpg", img)
-
-correctNegative = 0
-for i, sample in enumerate(test_negative):
-    img = cv.imread(f"{negativeTestDir}/{os.listdir(negativeTestDir)[i]}")
-    result = svm.predict(sample)[1]
-    if result == -1:
-        correctNegative += 1
-        cv.imwrite(f"resultsHOG/correct/negative_{i}.jpg", img)
-    else:
-        cv.imwrite(f"resultsHOG/incorrect/should_be_negative_{i}.jpg", img)
-
-
-totalTestCount = positiveTestCount + negativeTestCount
-totalCorrect = correctPositive + correctNegative
-accuracy = totalCorrect / totalTestCount
-print("Results\n----------------------------------")
-print(f"HOG cell side: {cellSide}")
-print(f"HOG descriptor size: {hog.getDescriptorSize()}")
-print(f"Resulting accuracy: {accuracy * 100} % ({totalCorrect}/{totalTestCount} samples)")
-print(f"{correctPositive}/{positiveTestCount} positive examples")
-print(f"{correctNegative}/{negativeTestCount} negative examples")
+    # for each part, iterate through original files and all parts of the images with the same size
+    for j, originalFileName in enumerate(os.listdir(originalDir)):
+        origImg = cv.imread(f"{originalDir}/{originalFileName}", 0)
+        origSize = getSizeFromShape(origImg.shape)
+        if DEBUG:
+            print(f"Original width: {origSize[0]}, height: {origSize[1]}")
+        for startX, startY, endX, endY in getSubsetsFromImage(croppedSize, origSize):
+            subset = origImg[startY:endY, startX:endX]
+            subsetSize = getSizeFromShape(subset.shape)
+            if DEBUG:
+                print(f"Subset: [{startX}, {startY}] -> [{endX}, {endY}]")
+                print(f"Subset width: {subsetSize[0]}, height: {subsetSize[1]}")
+            subsetDescriptor = hog.compute(subset)
+            print(f"Subset descriptor shape: {subsetDescriptor.shape}")
+            distance = np.linalg.norm(subsetDescriptor - partDescriptor)    # should calculate the euclidean distance
