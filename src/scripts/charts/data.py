9716261d9a84505f9f7690d5b8fe3a4e99076806
Lukáš Stuchlík <stuchlik1@seznam.cz>
2019-01-25 18:21:31 +0100

    Added algorithm for automatic chart generating from data

---
+++ src/scripts/charts/data.py
@@ -0,0 +1,84 @@
+# single measurement of collected data
+DATA_SINGLE = {
+    "totalTime": {
+        "HOG": 7740.5,
+        "FT": 872.91,
+        "SIFT": 618.98,
+        "SURF": 460.539,
+        "BRIEF": 230.885,
+        "ORB": 240.82,
+        "FREAK": 455.124
+    },
+    "descriptorPart": {
+        "HOG": 0.334,
+        "FT": 0.673,
+        "SIFT": 4.767,
+        "SURF": 1.375,
+        "BRIEF": 0.698,
+        "ORB": 1.195,
+        "FREAK": 1.634
+    },
+    "descriptorImage": {
+        "HOG": 5.248,
+        "FT": 7.644,
+        "SIFT": 47.002,
+        "SURF": 40.681,
+        "BRIEF": 7.024,
+        "ORB": 20.375,
+        "FREAK": 21.735
+    },
+    "matchingSingle": {
+        "HOG": 85.34,
+        "FT": 8.757,
+        "SIFT": 1.141,
+        "SURF": 0.425,
+        "BRIEF": 2.554,
+        "ORB": 0.694,
+        "FREAK": 3.185
+    },
+    "matchingAll": {
+        "HOG": 853.442,
+        "FT": 87.593,
+        "SIFT": 11.429,
+        "SURF": 4.273,
+        "BRIEF": 25.561,
+        "ORB": 6.953,
+        "FREAK": 31.874
+    },
+    "partProcess": {
+        "HOG": 853.887,
+        "FT": 88.319,
+        "SIFT": 16.254,
+        "SURF": 5.691,
+        "BRIEF": 26.311,
+        "ORB": 8.192,
+        "FREAK": 33.557
+    },
+    "descriptorPartSize": {
+        "HOG": 14464,
+        "FT": 128.11,
+        "SIFT": 12458.67,
+        "SURF": 2936.89,
+        "BRIEF": 4229.33,
+        "ORB": 3288,
+        "FREAK": 9737.14
+    },
+    "descriptorImageSize": {
+        "HOG": 197136,
+        "FT": 1444,
+        "SIFT": 74598.4,
+        "SURF": 39424,
+        "BRIEF": 51843.2,
+        "ORB": 13808,
+        "FREAK": 110316.8
+    },
+    "accuracy": {
+        "HOG": 1,
+        "FT": 1,
+        "SIFT": 1,
+        "SURF": 0.985,
+        "BRIEF": 0.5556,
+        "ORB": 0.2683,
+        "FREAK": 0.7778
+    }
+}
---
+++ src/scripts/charts/generateCharts.py
@@ -0,0 +1,106 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+import src.scripts.charts.data as data
+
+OUTPUT_DIR = "./output"
+Y_AXIS_TIME = "Čas [ms]"
+Y_AXIS_SIZE = "Velikost deskriptoru"
+Y_AXIS_ACCURACY = "Přesnost [%]"
+
+CHART_DATA = {
+    "totalTime": {
+        "title": "Celkový čas",
+        "yAxisLabel": Y_AXIS_TIME,
+        "type": "number"
+    },
+    "descriptorPart": {
+        "title": "Spočtení deskriptoru pro hledanou část",
+        "yAxisLabel": Y_AXIS_TIME,
+        "type": "number"
+    },
+    "descriptorImage": {
+        "title": "Spočtení deskriptoru pro jednotlivý obrázek",
+        "yAxisLabel": Y_AXIS_TIME,
+        "type": "number"
+    },
+    "matchingSingle": {
+        "title": "Hledání části v jednotlivém obrázku",
+        "yAxisLabel": Y_AXIS_TIME,
+        "type": "number"
+    },
+    "matchingAll": {
+        "title": "Hledání části ve všech obrázcích",
+        "yAxisLabel": Y_AXIS_TIME,
+        "type": "number"
+    },
+    "partProcess": {
+        "title": "Zpracování celé části",
+        "yAxisLabel": Y_AXIS_TIME,
+        "type": "number"
+    },
+    "descriptorPartSize": {
+        "title": "Průměrná velikost deskriptoru části",
+        "yAxisLabel": Y_AXIS_SIZE,
+        "type": "number"
+    },
+    "descriptorImageSize": {
+        "title": "Průměrná velikost deskriptoru obrázku",
+        "yAxisLabel": Y_AXIS_SIZE,
+        "type": "number"
+    },
+    "accuracy": {
+        "title": "Přesnost hledání",
+        "yAxisLabel": Y_AXIS_ACCURACY,
+        "type": "percentage"
+    },
+}
+VARIABLES = [
+    "totalTime",
+    "descriptorPart",
+    "descriptorImage",
+    "matchingSingle",
+    "matchingAll",
+    "partProcess",
+    "descriptorPartSize",
+    "descriptorImageSize",
+    "accuracy"
+]
+# default colors, can be hex RGB string
+COLORS = ["r", "g", "b", "c", "m", "y", "k"]
+
+def getVariableValues(variableName, dataset = data.DATA_SINGLE):
+    return list(dataset[variableName].values())
+
+def getVariableKeys(variableName, dataset = data.DATA_SINGLE):
+    return list(dataset[variableName].keys())
+
+def plotAndSave(dataset, variableName, filename=None, show=False):
+    algorithms = getVariableKeys(variableName, dataset)
+    values = getVariableValues(variableName, dataset)
+    fig, ax = plt.subplots()
+
+    index = np.arange(len(algorithms))
+
+    ax.bar(index, values, color=COLORS)
+    ax.set_xlabel("Algoritmus")
+    ax.set_ylabel(CHART_DATA[variableName]["yAxisLabel"])
+    if CHART_DATA[variableName]["type"] == "percentage":
+        ax.yaxis.set_major_formatter(FuncFormatter(lambda y, _: f"{(100 * y):.0f} %"))
+    ax.set_title(CHART_DATA[variableName]["title"])
+    ax.set_xticks(index)
+    ax.set_xticklabels(algorithms)
+    plt.grid(True, axis="y", zorder=0)
+
+    if show:
+        plt.show()
+    if not filename is None:
+        plt.savefig(os.path.abspath(f"{OUTPUT_DIR}/{filename}"))
+
+print("Generating charts...")
+
+for var in VARIABLES:
+    plotAndSave(data.DATA_SINGLE, var, f"single/{var}.png")
+
+print("Done!")
