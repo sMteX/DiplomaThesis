0e1d71851017c9b69ae6d2382cab74b3cf8022c8
Lukáš Stuchlík <stuchlik1@seznam.cz>
2019-01-28 17:20:45 +0100

    Added experiment chart with 4 y axes

--- src/scripts/charts/generateCharts.py
+++ src/scripts/charts/generateCharts.py
@@ -148,6 +148,65 @@ def plotAndSaveTwinAxes(dataset, variableName, filename=None, show=False):
     if not filename is None:
         plt.savefig(os.path.abspath(f"{OUTPUT_DIR}/{filename}"))
 
+def testQuadrupleYAxis():
+    COLORS2 = ["r", "g", "b", "c", "m", "y", "k"]
+    def make_patch_spines_invisible(ax):
+        ax.set_frame_on(True)
+        ax.patch.set_visible(False)
+        for sp in ax.spines.values():
+            sp.set_visible(False)
+
+    fig, left1 = plt.subplots()
+
+    right1 = left1.twinx()
+    left2 = left1.twinx()
+    right2 = left1.twinx()
+
+    make_patch_spines_invisible(right1)
+    make_patch_spines_invisible(right2)
+    make_patch_spines_invisible(left2)
+
+    right2.spines["right"].set_position(("axes", 1.2))
+    left2.spines["left"].set_position(("axes", -0.2))
+
+    right2.spines["right"].set_visible(True)
+    left2.spines["left"].set_visible(True)
+    left2.yaxis.set_label_position("left")
+    left2.yaxis.set_ticks_position("left")
+
+    labels = getVariableKeys("matchingSingle")
+    matchingAll = getVariableValues("matchingAll", data.DATA_SINGLE)
+    matchingSingle = getVariableValues("matchingSingle", data.DATA_SINGLE)
+
+    barWidth = 0.35
+
+    index = np.arange(len(labels))
+
+    left1.set_title("Hledání části")
+    left1.set_xlabel("Algoritmus")
+    left1.set_xticks(index)
+    left1.set_xticklabels(labels)
+
+    left1.set_ylabel("v jednotlivém obrázku (HOG)")
+    left2.set_ylabel("ve všech obrázcích (HOG)")
+    right1.set_ylabel("v jednotlivém obrázku")
+    right2.set_ylabel("ve všech obrázcích")
+
+    left2.bar(index[:1] - barWidth/2, matchingAll[:1], barWidth, color=COLORS2[:1])
+    left1.bar(index[:1] + barWidth/2, matchingSingle[:1], barWidth, color=COLORS[:1])
+    right2.bar(index[1:] - barWidth / 2, matchingAll[1:], barWidth, color=COLORS2[1:])
+    right1.bar(index[1:] + barWidth / 2, matchingSingle[1:], barWidth, color=COLORS[1:])
+
+    tkw = dict(size=4, width=1.5)
+    left1.tick_params(axis='y', **tkw)
+    right1.tick_params(axis='y', **tkw)
+    right2.tick_params(axis='y', **tkw)
+    left2.tick_params(axis='y', **tkw)
+    left1.tick_params(axis='x', **tkw)
+
+    plt.tight_layout()
+    plt.grid(True, axis="y")
+    plt.show()
 
 print("Generating charts...")
 
