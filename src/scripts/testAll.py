9f94b1ac2b3a77f8b1d604851fa2be528984619d
Lukáš Stuchlík <stuchlik1@seznam.cz>
2019-02-01 00:52:53 +0100

    Added timing to the testAll script. Re-run the fixed algorithms, results seem correct this time. Committed only first iteration (image results).

--- src/scripts/testAll.py
+++ src/scripts/testAll.py
@@ -1,4 +1,5 @@
 import gc
+from time import strftime
 from collections import namedtuple
 from src.scripts.algorithms.BaseAlgorithm import fromDirectory
 from src.scripts.algorithms.HOG import HOG
@@ -26,18 +27,18 @@ algorithms = [
     Algorithm(name="HOG",   type=HOG,   output=f"{outputDir}/hog")
 ]
 
-print("Started")
+print(f"({strftime('%H:%M:%S')}) Started")
 
 for a in algorithms:
-    print(f"Algorithm: {a.name}")
+    print(f"({strftime('%H:%M:%S')}) Algorithm: {a.name}")
     if a.name == "HOG":
         print("Get yourself a cup of coffee, this will take almost an hour")
     for i in range(10):
-        print(f"- Iteration {i + 1}")
+        print(f"({strftime('%H:%M:%S')}) - Iteration {i + 1}")
         obj = a.type(parts=fromDirectory(partsDir), images=fromDirectory(originalDir))
         obj.process()
         obj.writeResults(f"{a.output}/{i}", includePart=True)
         obj.printResults(f"{a.output}/{i}_result.txt")
         gc.collect()
 
-print("Ended")
+print(f"({strftime('%H:%M:%S')}) Ended")
