4be41c91b9b612ddd136062be7bcd6e2e7d350cf
Lukáš Stuchlík <stuchlik1@seznam.cz>
2019-05-11 19:35:16 +0200

    Bit of comments in the lighting test script

--- src/testFtLighting.py
+++ src/testFtLighting.py
@@ -15,35 +15,41 @@ partsDir = f"{dataDir}/parts/300x300"
 originalDir = f"{dataDir}/original/300x300"
 outputDir = f"{dataDir}/experimentResults/lighting"
 
+# load annotations - they contain where each part is
 annotations = None
 with open(f"{dataDir}/parts/300x300_annotations.json", "r") as file:
     annotations = json.load(file, object_hook=lambda d: namedtuple("X", d.keys())(*d.values()))
 
+# construct absolute paths for all parts
 absolute = os.path.abspath(partsDir)
 paths = []
 for fileName in os.listdir(absolute):
     path = os.path.abspath(f"{absolute}/{fileName}")
     paths.append(path)
 
+# helper methods for formatting
 formatPercentage = lambda x: f"+{x} %" if x >= 0 else f"{x} %"
 formatFileName = lambda i, b, c: f"part{i}_b{b}_c{c}.jpg"
 
 print(f"({strftime('%H:%M:%S')}) Started")
-j = 0
-step = 5
-total = float(((160 / step) + 1)**2)
-countDict = { b: {c: 0 for c in range(-80, 81, step)} for b in range(-80, 81, step)}
+progress = 0
+step = 5    # step in brightness/contrast in %
+total = float(((160 / step) + 1)**2)    # total amount of tests
+countDict = { b: {c: 0 for c in range(-80, 81, step)} for b in range(-80, 81, step)}    # constructs dictionary for counts for each combination of brightness and contrast
 
 formatProgress = lambda x: f"{(100 * x / total):.0f} %"
+
 for b in range(-80, 81, step):
     for c in range(-80, 81, step):
-        j += 1
-        print(f"({strftime('%H:%M:%S')}) ({formatProgress(j)}) Brightness: {formatPercentage(b)}, Contrast: {formatPercentage(c)}")
+        progress += 1
+        print(f"({strftime('%H:%M:%S')}) ({formatProgress(progress)}) Brightness: {formatPercentage(b)}, Contrast: {formatPercentage(c)}")
+        # preprocesses parts - changes brightness/contrast according to b or c
         images = []
         for path in paths:
             img = cv.imread(path)
             alpha = 1 + float(c)/100
             beta = float(b)/100 * 255
+            # clips the values at 0 - 255, converts to unsigned ints (0 - 255)
             image = InputImage(np.asarray(np.clip(img * alpha + beta, 0, 255), dtype=np.uint8), path)
             images.append(image)
 
@@ -52,6 +58,7 @@ for b in range(-80, 81, step):
 
         for i, result in enumerate(results):
             _path = None
+            # finds correct result in annotations
             annotatedResult = next(x for x in annotations.matches if x.part == os.path.basename(result.partPath))
             if annotatedResult is None or os.path.basename(result.imagePath) != annotatedResult.image:
                 _path = f"{outputDir}/nope/{formatFileName(i, b, c)}"
